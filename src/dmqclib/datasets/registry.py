23eac41fd8264bfeef024d01692094eeeac27974
Takaya Saito <takaya.saito@hi.no>
2025-06-20 03:18:27 +0200

    Create class loader function for extract dataset classes

--- src/dmqclib/datasets/registry.py
+++ src/dmqclib/datasets/registry.py
@@ -1,6 +1,7 @@
 from dmqclib.datasets.input.dataset_a import InputDataSetA
 from dmqclib.datasets.select.dataset_a import SelectDataSetA
 from dmqclib.datasets.locate.dataset_a import LocateDataSetA
+from dmqclib.datasets.extract.dataset_a import ExtractDataSetA
 
 INPUT_DATASET_REGISTRY = {
     "InputDataSetA": InputDataSetA,
@@ -12,4 +13,8 @@ SELECT_DATASET_REGISTRY = {
 
 LOCATE_DATASET_REGISTRY = {
     "LocateDataSetA": LocateDataSetA,
+}
+
+EXTRACT_DATASET_REGISTRY = {
+    "ExtractDataSetA": ExtractDataSetA,
 }
